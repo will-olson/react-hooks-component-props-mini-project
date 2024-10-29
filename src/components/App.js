3c8f24ca1ab6b24c4d15926a6f9d7db2769f4a95
Will Olson <will.olson.b@gmail.com>
2024-10-29 16:20:59 -0500

    Built out components, minus the bonus feature

--- src/components/App.js
+++ src/components/App.js
@@ -5,9 +5,31 @@ console.log(blogData);
 
 function App() {
   return (
-    <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+    <div>
+      <div className="Header">
+        <h1>{blogData.name}</h1>
+        You're on your own from here! Follow the deliverables; test things out in
+        the browser as you write your code; and good luck!
+      </div>
+      <div className="About">
+        <aside>
+          <img
+            src={blogData.image || "https://via.placeholder.com/215"}
+            alt="blog logo"
+          />
+        </aside>
+      </div>
+      <div className="Article List">
+        <main>
+          {blogData.posts.map(post => (
+            <article key={post.id}>
+              <h3>{post.title}</h3>
+              <small>{post.date || "January 1, 1970"}</small>
+              <p>{post.preview}</p>
+            </article>
+          ))}
+        </main>
+      </div>
     </div>
   );
 }
